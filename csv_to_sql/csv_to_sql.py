a2ee7aa75ab6a405d0603d1d8af28bcf8f6d6b21
jefferu97 <jeffersondch@gmail.com>
2024-10-03 14:23:28 -0600

    adding the complete cst to sql script

--- csv_to_sql/csv_to_sql.py
+++ csv_to_sql/csv_to_sql.py
@@ -14,8 +14,20 @@ column_name_mapping = {
     'Fecha de Caducidad': 'expiration_date'
 }
 
-with open(os.path.join(__location__, 'ingredientes.csv'), newline='') as csvfile:
+with open(os.path.join(__location__, 'ingredientes.csv'), newline='', encoding='utf-8') as csvfile:
     ingredients_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
     headers, *ingredients = ingredients_reader
+    mapped_headers = ", ".join(list(map(lambda header: column_name_mapping[header],headers)))
+    final_string = ""
+    for sub_array in ingredients:
+        values_string= ""
+        values_string += f"({', '.join(sub_array)})"
+        final_string += values_string + ',' + '\n'
+    statement = f"INSERT INTO Ingredients ({mapped_headers})" +'\n'  + "VALUES" +'\n' + final_string[:-2] +';' +'\n' + "COMMIT;"
+    #print(statement)
+    with open("transaction.sql", "w") as file:
+      file.writelines(statement)
+    # sql_statement = INSERT INTO Ingredients (CustomerName, ContactName, Address, City, PostalCode, Country)
+    # VALUES ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway');
     # for row in ingredients_reader:
     #     print(row)
